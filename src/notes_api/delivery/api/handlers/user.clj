683fc21e11238d0000a8172654ee048d23720800
Clauber Camilo <clauber_camilo@live.com>
2022-08-23 22:30:17 -0300

    feat(create-user): add valid user method

--- src/notes_api/delivery/api/handlers/user.clj
+++ src/notes_api/delivery/api/handlers/user.clj
@@ -1,11 +1,27 @@
 (ns notes-api.delivery.api.handlers.user
   (:require
-   [io.pedestal.log :as log]))
+   [io.pedestal.log :as log]
+   [clojure.string :as s]))
 
-(defn validate-user [user] (and
-                            (seq user)
-                            (seq (:name user))))
+(def qualquer-coisa {:name "jose"
+                     :password "12345"
+                     :email "jose@jose.com"})
 
-(defn create-user-handler [{:keys [json-params]}] (let [created-user (let [is-valid (validate-user json-params)]
-                                                                       (log/info :xablau is-valid))]))
+(defn not-blank [s] (not (s/blank? s)))
 
+(defn valid-user? [{:keys [name
+                           password
+                           email]}]
+  (boolean
+   (and
+    (not-blank name)
+    (not-blank password)
+    (not-blank email))))
+
+(defn create-user-handler [{{:keys [user]} :json-params :as request}]
+  (def x request)
+  (if (valid-user? user)
+    (log/info :create-user "foi")
+    (log/error :create-user "to bad")))
+
+(prn x)
