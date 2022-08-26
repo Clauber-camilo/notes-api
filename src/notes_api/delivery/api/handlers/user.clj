ea1f7bc0eb7b60251991ca67d1c1c7d4a2a3c60f
Kaue Schultz <kaue.schultz@beacon.works>
2022-08-26 12:26:06 -0300

    add: malli examples

--- src/notes_api/delivery/api/handlers/user.clj
+++ src/notes_api/delivery/api/handlers/user.clj
@@ -1,22 +1,68 @@
 (ns notes-api.delivery.api.handlers.user
   (:require
-   [io.pedestal.log :as log]
-   [clojure.string :as s]))
-
-(def qualquer-coisa {:name "jose"
-                     :password "12345"
-                     :email "jose@jose.com"})
+    [clojure.string :as s]
+    [clojure.string]
+    [io.pedestal.log :as log]
+    [malli.core :as m]
+    [malli.error :as me]))
 
 (defn not-blank [s] (not (s/blank? s)))
+
+(def name-pattern #"^.{8,}$")
+(def password-pattern #"(?=^.{10,}$)(?=.*(\@|\!))")
+(def email-pattern #"^[a-z0-9]+@.[a-z]")
+
+
+(def user-schema
+  [:map {:closed true}
+   [:name name-pattern]
+   [:password
+    [:re {:error/message "Must have more than 10 characters and at least one of '@','!'"}
+     password-pattern]]
+   [:email email-pattern]
+   [:additional-info {:optional true}
+    [:map
+     [:location
+      [:map {:closed true}
+       [:lat double?]
+       [:long double?]]]]]])
+
+(comment
+  ;;validate data using a schema
+  (m/validate user-schema {:name "Kaue Matos"
+                           :password "12345678@9"
+                           :email "somemail@fubanga.com"})
+
+  ;;define a validator function for a fixed schema
+  ;;more time-efficient
+  (def valid-user? (m/validator user-schema))
+  (valid-user? {:name "Kaue Matos"
+                :password "12345678@9"
+                :email "somemail@fubanga.com"})
+
+  ;;Explain what's wrong
+  (def error-data
+    (m/explain user-schema
+               {:name "Kaue Matos"
+                :password "burundanga"
+                :email "paranaue.com"
+                :jabiraca 72
+                :additional-info {:location {:lat 22
+                                             :milambe true
+                                             :long -4.255}}}))
+
+  ;;Explain error in human readable format
+  (me/humanize error-data))
+
 
 (defn valid-user? [{:keys [name
                            password
                            email]}]
   (boolean
-   (and
-    (not-blank name)
-    (not-blank password)
-    (not-blank email))))
+    (and
+      (not-blank name)
+      (not-blank password)
+      (not-blank email))))
 
 (defn create-user-handler [{{:keys [user]} :json-params :as request}]
   (def x request)
