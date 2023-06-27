f20a25d8acdacb63fa2682b3235ff77221851ad4
Clauber Camilo <clauber_camilo@live.com>
2023-06-27 13:34:27 -0300

    feat(migration): update user migration

--- resources/migrations/20230130210057-create-user.up.sql
+++ resources/migrations/20230130210057-create-user.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
   id uuid primary key,
   name varchar(32),
   email varchar(255) unique,
+  level varchar(100) default 'free',
   password varchar(255),
   created_at timestamp with time zone default NOW(),
   updated_at timestamp with time zone default NOW()
