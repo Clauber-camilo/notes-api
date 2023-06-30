be0648b54161e9afe699563bcdd70615d7582751
Clauber Camilo <clauber_camilo@live.com>
2023-06-30 19:18:11 -0300

    feat(auth): improve auth method and treat the errors

--- src/notes_api/auth/core.clj
+++ src/notes_api/auth/core.clj
@@ -1,5 +1,8 @@
 (ns notes-api.auth.core
-  (:require [io.pedestal.log :as log]
+  (:require [crypto.password.bcrypt :as crypto]
+            [io.pedestal.log :as log]
+            [malli.core :as m]
+            [malli.error :as me]
             [notes-api.user.core :refer [get-user-by-email]]
             [notes-api.utils :refer [email-pattern password-pattern]]))
 
@@ -10,13 +13,37 @@
      password-pattern]]
    [:email email-pattern]])
 
+(def valid-auth-body? (m/validator schema))
+(defn humanize-error
+  [auth-body]
+  (->
+   (m/explain schema auth-body)
+   (me/humanize)))
+
 (defn authenticate-user
   "Authenticate user"
   [email password db]
-  (let [user (get-user-by-email email db)]
-    (log/info :query user)
-    (if (= (count user) 0)
-      (do (log/error :zica "nao achou")
-          {:error "User not found"})
-      (do (log/info :zica user)
-          {:success user}))))
+  (let [user (first (get-user-by-email email db))]
+    (cond
+      (not (valid-auth-body? {:email email :password password}))
+      {:error (humanize-error {:email email :password password}) :status 500}
+
+      (= (count user) 0)
+      {:error "User not found" :status 404}
+
+      (not (crypto/check password (:users/password user)))
+      {:error "Something went wrong" :status 500}
+
+      :else
+      {:success {:name (:users/name user)
+                 :email (:users/email user)
+                 :level (:users/level user)
+                 :created_at (:users/created_at user)
+                 :updated_at (:users/updated_at user)}
+       :status 200})))
+
+(comment
+  (require '[notes-api.database :refer [db]])
+  (authenticate-user "test@test.com" "123456678910@"
+                     #_{:clj-kondo/ignore [:unresolved-symbol]}
+                     (db)))
--- src/notes_api/http/handlers/auth.clj
+++ src/notes_api/http/handlers/auth.clj
@@ -1,33 +1,18 @@
 (ns notes-api.http.handlers.auth
   (:require [cheshire.core :as json]
-            [io.pedestal.log :as log]
-            [malli.core :as m]
-            [malli.error :as me]
-            [notes-api.auth.interface :as auth]
-            [ring.util.response :as ring-resp]))
-
-(def valid-auth-body? (m/validator auth/schema))
-
-(defn humanize-error
-  [auth-body]
-  (->
-   (m/explain auth/schema auth-body)
-   (me/humanize)))
+            [notes-api.auth.interface :as auth]))
 
 (defn authenticate-user
   "[POST] authenticate-user"
   [{:keys [json-params system-context]}]
-  (if (valid-auth-body? json-params)
-    (-> (auth/authenticate-user (assoc json-params :db system-context))
-        (#(cond
-            (contains? % :error) {:status 404 :body (json/generate-string %)}
-            (contains? % :success) {:status 200 :body (json/generate-string (:success %))})))
-    (do
-      (log/error :authenticate-user "This isn't a valid body")
-      (ring-resp/bad-request (json/generate-string {:error (humanize-error json-params)})))))
+  (let [auth-result (auth/authenticate-user (assoc json-params :db system-context))
+        response (cond
+                   (:error auth-result)
+                   (assoc auth-result :body (:error auth-result))
 
-(comment
-  (do
-    (def xuser {:email "teste@teste.com" :password "1234567890!"})
-    (valid-auth-body? xuser)
-    (humanize-error xuser)))
+                   (:success auth-result)
+                   (assoc auth-result :body (:success auth-result))
+
+                   :else
+                   (assoc auth-result :body (:error auth-result)))]
+    (update response :body json/generate-string (:body response))))
